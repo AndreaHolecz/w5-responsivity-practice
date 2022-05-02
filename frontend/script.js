7168fb3a646c6ca803ff4ed533da3d11e6876476
AndreaHolecz <andreaholecz94@gmail.com>
2022-05-02 10:25:03 +0200

    wow

---
+++ frontend/script.js
@@ -0,0 +1,38 @@
+const headerComponent = function(title){
+    return `
+    <header>${title}</header>
+    `
+}
+
+const sectionComponent = function(id, buttonText, h2Text){
+    return `
+    <section id="${id}">
+        <h2>${h2Text}</h2>
+        <button>${buttonText}</button>
+    </section>
+    `
+}
+
+const footerComponent = function(){
+    return `
+    <footer></footer>
+    `
+}
+
+const loadEvent = function (){
+    const rootElement = document.getElementById("root");
+
+    rootElement.insertAdjacentHTML("beforebegin", headerComponent("Responsivity practice"));
+
+    let sections = ""
+
+    for (let i = 1; i < 5; i++) {
+        sections += sectionComponent (`id-${i}`, `Button ${i}`, `Subtitle ${i}`)  
+    }
+
+    rootElement.insertAdjacentHTML("beforeend", sections);
+
+    rootElement.insertAdjacentHTML("afterend", footerComponent());
+}
+
+window.addEventListener("load", loadEvent)
